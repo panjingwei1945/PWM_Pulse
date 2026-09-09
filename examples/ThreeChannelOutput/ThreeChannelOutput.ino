eb504bceb11c4a1c79036bbdb7e7559a182892c7
panjingwei1945 <panjingwei1945@hotmail.com>
2026-09-09 15:59:51 +0800

    modify example2

--- examples/ThreeChannelOutput/ThreeChannelOutput.ino
+++ examples/ThreeChannelOutput/ThreeChannelOutput.ino
@@ -20,9 +20,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(MARKER_PIN, OUTPUT);
   digitalWrite(MARKER_PIN, LOW);
-  PWM_PULSE.p1_init();
-  PWM_PULSE.p2_init();
-  PWM_PULSE.p3_init();
+  PWM_PULSE.init(); // Initialize all three channels.
 }
 
 void loop() {
