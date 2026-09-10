7b4f2b4bc4bb7ea0699625db3b260556917d57d6
panjingwei1945 <panjingwei1945@hotmail.com>
2026-09-10 15:34:44 +0800

    modify version to v0.4.1

--- PWM_Pulse.cpp
+++ PWM_Pulse.cpp
@@ -1,4 +1,4 @@
-/* Version v0.3.6
+/* Version v0.4.1
 * create in 20141119
 * Modified in 20260908
 * This library generates 3 parallel PWM-modulated pulse outputs on ATmega2560.
--- PWM_Pulse.h
+++ PWM_Pulse.h
@@ -1,4 +1,4 @@
-/* Version v0.3.6
+/* Version v0.4.1
  * create in 20141119
  * Modified in 20260908
  * Three PWM-modulated pulse outputs on ATmega2560, configured for 16 MHz.
